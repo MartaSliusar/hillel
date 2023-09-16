e1aa5139ecaf6a7ed4ea9fa74fcd8a07f1bd3d6e
marta <sliusar18@gmail.com>
2023-09-16 17:27:07 +0300

    corect homework_09

--- lesson_09/homework_09.py
+++ lesson_09/homework_09.py
@@ -11,28 +11,39 @@ class Price:
         self.currency: str = currency
 
     def add(self, other):
-        if self.currency == other.currency:
-            return Price(self.amount + other.amount, "USD")
-        else:
-            return Price(self.convert_to_usd() + other.convert_to_usd(), "USD")
+        total_amount = (
+            self.convert_to_usd().amount + other.convert_to_usd().amount
+        )
+        return Price(
+            self.convert_from_usd(total_amount, self.currency).amount,
+            self.currency,
+        )
 
     def subtract(self, other):
-        if self.currency == other.currency:
-            return Price(self.amount - other.amount, "USD")
-        else:
-            return Price(self.convert_to_usd() - other.convert_to_usd(), "USD")
+        total_amount = (
+            self.convert_to_usd().amount - other.convert_to_usd().amount
+        )
+        return Price(
+            self.convert_from_usd(total_amount, self.currency).amount,
+            self.currency,
+        )
 
     def convert_to_usd(self):
         if self.currency == "USD":
-            return self.amount
+            return self
         else:
-            return self.amount * current_exchange_rate[self.currency]
+            return Price(
+                self.amount * current_exchange_rate[self.currency], "USD"
+            )
 
     def convert_from_usd(self, usd_amount, target_currency):
         if target_currency == "USD":
-            return usd_amount
+            return Price(usd_amount, "USD")
         else:
-            return usd_amount * current_exchange_rate[target_currency]
+            return Price(
+                usd_amount / current_exchange_rate[target_currency],
+                target_currency,
+            )
 
 
 price1 = Price(100, "UAH")
@@ -40,9 +51,8 @@ price2 = Price(1, "EUR")
 result_add = price1.add(price2)
 result_subtract = price1.subtract(price2)
 
-
 print(f"Result of addition: {result_add.amount} {result_add.currency}")
 print(
-    f"Result of subtraction: {result_subtract.amount} "
+    f"Result of subtraction: {result_subtract.amount}"
     f"{result_subtract.currency}"
 )
